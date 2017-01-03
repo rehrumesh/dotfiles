fd1f0a5d9b0cb39a928cadc196b99aeb3b689ff9
Rumesh Eranga Hapuarachchi <rehrumesh@users.noreply.github.com>
2017-01-03 22:16:32 +0530

    Added youcompleteme, tern setup

--- setup.sh
+++ setup.sh
@@ -3,3 +3,11 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln ./vim/.vimrc ~/.vimrc
 vim +PluginInstall
+
+npm install -g typescript
+
+cd ~/.vim/bundle/tern_for_vim
+npm install
+
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer --tern-completer
